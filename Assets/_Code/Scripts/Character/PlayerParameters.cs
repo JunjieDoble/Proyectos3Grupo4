9fbf5cca1b89e29e27eee7118a8a8059bd68f1d8
ShadowLoker <lluckoomecebria@gmail.com>
2026-04-15 15:39:55 +0200

    Create Player Parameters Scriptable Object

--- Assets/_Code/Scripts/Character/PlayerParameters.cs
+++ Assets/_Code/Scripts/Character/PlayerParameters.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerParameters", menuName = "Scriptable Objects/PlayerParameters")]
-public class PlayerParameters : ScriptableObject
+namespace _Code.Scripts.Character
 {
-    
+    [CreateAssetMenu(fileName = "PlayerParameters", menuName = "Parameters/Player")]
+    public class PlayerParameters : ScriptableObject
+    {
+        [Header("Speeds")]
+        public float walkSpeed = 5f;
+        public float runSpeed = 10f;   
+        public float crouchSpeed = 2.5f;        
+        public float jumpSpeed = 8f;     
+        
+        [Header("Heights")]
+        public float walkHeight = 2f;       
+        public float crouchHeight = 1f;
+    }
 }
