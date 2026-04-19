b2b5cf1c5332a16abe2a2bad1dc5581c73591e0e
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-04-19 17:59:47 +0200

    Enemy chase player

---
+++ Assets/Resources/Behaviour/EnemyBehaviour/LineOfSightCheckCondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Behavior;
+using UnityEngine;
+
+[Serializable, Unity.Properties.GeneratePropertyBag]
+[Condition(name: "LineOfSightCheck", story: "Check [target] with [LineOfSightDetector]", category: "Conditions", id: "023d879baeecfedaf3cb9f66ba6dbbcc")]
+public partial class LineOfSightCheckCondition : Condition
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<LineOfSightDetector> LineOfSightDetector;
+
+    public override bool IsTrue()
+    {
+        return LineOfSightDetector.Value.PerformDetection(Target.Value) != null;
+    }
+}
---
+++ Assets/Resources/Behaviour/EnemyBehaviour/RangeDetector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class RangeDetector : MonoBehaviour
+{
+    [SerializeField] private float detectionRange = 10f;
+    [SerializeField] private LayerMask detectionMask;
+    [SerializeField] private bool showDebugVisuals = true;
+
+    public GameObject DetectedTarget { get; private set; }
+
+    public GameObject UpdateDetector()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRange, detectionMask);
+
+        if (colliders.Length > 0)
+        {
+            DetectedTarget = colliders[0].gameObject;
+        }
+        else
+        {
+            DetectedTarget = null;
+        }
+
+        return DetectedTarget;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showDebugVisuals && !enabled) return;
+
+        Gizmos.color = DetectedTarget != null ? Color.red : Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+}
