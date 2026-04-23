88eba20c9fcba79d3c4863e3b9faed9ef93f78b4
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-04-23 13:27:33 +0200

    Prepare enemy for interaction

--- Assets/_Code/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/_Code/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour, IEnemy
 {
     [Header("Parameters")]
-    [SerializeField] public Transform[] patrolPoints;
-    [SerializeField] public float speed = 2f;
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private float speed = 2f;
     [SerializeField] private float detectionRadius = 10f;
+    [SerializeField] private float alertRadius = 15f;
     [SerializeField] private float detectionAngle = 180f;
+    [SerializeField] private LayerMask playerLayerMask;
 
     private Animator _animator;
     private GameObject _player;
     private bool _isDead = false;
     private Color _gizmosColor = Color.green;
+    private Vector3 _lastAlertPosition;
 
     private void Start()
     {
@@ -28,19 +31,15 @@ public class EnemyBehaviour : MonoBehaviour
 
     private void DistanceAndVisionToPlayer()
     {
-        Vector3 playerPos = _player.transform.position;
-        Vector3 enemyPos = transform.position;
-
-        Vector2 playerXZ = new Vector2(playerPos.x, playerPos.z);
-        Vector2 EnemyXZ = new Vector2(enemyPos.x, enemyPos.z);
-        float HorizontalDistance = Vector2.Distance(playerXZ, EnemyXZ);
+        float HorizontalDistance = GetDistanceToPlayer();
         _animator.SetFloat("DistanceToPlayer", HorizontalDistance);
 
-        Vector3 directionToPlayer = _player.transform.position - transform.position;
-        Vector3 directionForward = transform.forward;
-        float angleToPlayer = Vector3.Angle(directionForward, directionToPlayer);
+        float angleToPlayer = GetAngleToPlayer();
 
-        if (HorizontalDistance <= detectionRadius && angleToPlayer <= detectionAngle / 2f)
+        RaycastHit hit;
+        Vector3 directionToPlayer = _player.transform.position - transform.position;
+        Physics.Raycast(transform.position, directionToPlayer, out hit, detectionRadius);
+        if (hit.collider != null && hit.collider.gameObject == _player && angleToPlayer <= detectionAngle/2f && HorizontalDistance <= detectionRadius)
         {
             _animator.SetBool("SeePlayer", true);
             _gizmosColor = Color.red;
@@ -52,12 +51,28 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
-    private void EnemyDie()
+    public void KillEnemy()
     {
-        _isDead = true;
-        _animator.SetTrigger("Die");
+        float angleToPlayer = GetAngleToPlayer();
 
-        Destroy(gameObject);
+        // Detect if the player is behind the enemy when killing it
+        if (angleToPlayer >= detectionAngle /2f && !_isDead)
+        {
+            _isDead = true;
+            _animator.SetTrigger("Die");
+
+            Destroy(gameObject);
+        }
+    }
+
+    public void AlertEnemy(Vector3 alertPosition)
+    {
+        float distanceToAlert = Vector3.Distance(transform.position, alertPosition);
+        if (distanceToAlert <= alertRadius && !_isDead)
+        {
+            _lastAlertPosition = alertPosition;
+            _animator.SetBool("Alerted", true);
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -73,5 +88,37 @@ public class EnemyBehaviour : MonoBehaviour
         Gizmos.color = _gizmosColor;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
         Gizmos.DrawRay(transform.position, transform.forward * detectionRadius);
+    }
+
+    private float GetDistanceToPlayer()
+    {
+        Vector3 playerPos = _player.transform.position;
+        Vector3 enemyPos = transform.position;
+
+        Vector2 playerXZ = new Vector2(playerPos.x, playerPos.z);
+        Vector2 EnemyXZ = new Vector2(enemyPos.x, enemyPos.z);
+        return Vector2.Distance(playerXZ, EnemyXZ);
+    }
+
+    private float GetAngleToPlayer()
+    {
+        Vector3 directionToPlayer = _player.transform.position - transform.position;
+        Vector3 directionForward = transform.forward;
+        return Vector3.Angle(directionForward, directionToPlayer);
+    }
+
+    public Transform[] GetPatrolPoints()
+    {
+        return patrolPoints;
+    }
+
+    public float GetSpeed()
+    {
+            return speed;
+    }
+
+    public Vector3 GetLastAlertPosition()
+    {
+        return _lastAlertPosition;
     }
 }
