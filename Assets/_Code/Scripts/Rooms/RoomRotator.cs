6d7d7e1dfd05b29225b7e623b5924acff05b0f98
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-05-02 12:52:41 +0200

    Fix rotation bugs

--- Assets/_Code/Scripts/Rooms/RoomRotator.cs
+++ Assets/_Code/Scripts/Rooms/RoomRotator.cs
@@ -1,14 +1,17 @@
 ﻿using _Code.Scripts.Rooms;
 using UnityEngine;
 using Interactions;
+using System.Collections;
 
 namespace Rooms
 {
     public class RoomRotator : MonoBehaviour, IHoldInteractable, ILockable
     {
         [SerializeField] private Room targetRoom;
+        [SerializeField] private float rotationCooldown = 1.5f;
 
         private IInteractor _currentInteractor;
+        private Coroutine _rotationCoroutine;
 
         public void Awake()
         {
@@ -28,22 +31,29 @@ namespace Rooms
 
         public void OnHoldStarted(IInteractor interactor)
         {
+            if (_currentInteractor != null && _currentInteractor != interactor) return;
+
             _currentInteractor = interactor;
             RotateRoom();
+
+            if (_rotationCoroutine != null) StopCoroutine(_rotationCoroutine);
+            _rotationCoroutine = StartCoroutine(RotationCooldown());
         }
 
         public void OnHoldCanceled(IInteractor interactor)
         {
-            if (_currentInteractor != interactor) return;
-
+            if (_currentInteractor != null && _currentInteractor != interactor) return;
             targetRoom?.CancelRotate();
+
+            if (_rotationCoroutine != null) StopCoroutine(_rotationCoroutine);
             _currentInteractor = null;
         }
 
         public void OnHoldCompleted(IInteractor interactor)
         {
-            if (_currentInteractor != interactor) return;
+            if (_currentInteractor != null && _currentInteractor != interactor) return;
 
+            if (_rotationCoroutine != null) StopCoroutine(_rotationCoroutine);
             _currentInteractor = null;
         }
 
@@ -60,6 +70,14 @@ namespace Rooms
         public void Unlock()
         {
             // TODO: Implement unlock
+        }
+
+        private IEnumerator RotationCooldown()
+        {
+            yield return new WaitForSeconds(rotationCooldown);
+
+            _currentInteractor = null;
+            _rotationCoroutine = null;
         }
     }
 }
