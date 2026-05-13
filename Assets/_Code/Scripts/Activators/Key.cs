887bcd268fdf1afda0bd8747a7a2b18578be1f8e
ShadowLoker <lluckoomecebria@gmail.com>
2026-05-13 16:00:19 +0200

    Multi Activable Activators and new keys

--- Assets/_Code/Scripts/Activators/Key.cs
+++ Assets/_Code/Scripts/Activators/Key.cs
@@ -5,11 +5,13 @@ namespace _Code.Scripts.Activators
 {
     public class Key : Activator, IInteractable
     {
+        private static Key _activeKey;
         public void Interact(IInteractor interactor)
         {
             SetActive(true);
+            _activeKey?.SetActive(false);
+            _activeKey = this;
             onActivatorUpdate?.Invoke();
-            Destroy(gameObject);
         }
     }
 }
