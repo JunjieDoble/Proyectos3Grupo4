530b64298e3dde2554cb89d5d1b8852690d49663
Pau "Katsu" Castellà i Reig <paucastella2004@gmail.com>
2026-05-15 14:26:49 +0200

    Add Checkpoint On Trigger

--- Assets/_Code/Scripts/CheckPoint/Checkpoint.cs
+++ Assets/_Code/Scripts/CheckPoint/Checkpoint.cs
@@ -2,42 +2,50 @@ using System;
 using _Code.Scripts.Character;
 using _Code.Scripts.Interactions;
 using Interactions;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _Code.Scripts.CheckPoint
 {
-    public class Checkpoint : MonoBehaviour, IInteractable
-    {
+    public class Checkpoint : MonoBehaviour, IInteractable {
         public static Action OnCheckpointChange;
         private static Checkpoint CurrentCheckpoint { get; set; }
         private Vector3 _spawnPosition;
         private Player _player;
 
         public void Awake() {
-            if (CurrentCheckpoint == null)
-            {
+            if (CurrentCheckpoint == null) {
                 CurrentCheckpoint = this;
                 _player = FindFirstObjectByType<Player>();
                 _spawnPosition = _player.transform.position;
             }
         }
 
-        public void Interact(IInteractor interactor)
-        {
-            if (interactor is PlayerInteractor playerInteractor)
-            {
+        public void Interact(IInteractor interactor) {
+            if (interactor is PlayerInteractor playerInteractor) {
                 OnCheckpointChange?.Invoke();
                 CurrentCheckpoint = this;
                 _spawnPosition = playerInteractor.Transform.position;
                 _player = playerInteractor.GetComponent<Player>();
             }
         }
-    
-        public static void RespawnPlayer()
-        {
+
+        public static void RespawnPlayer() {
             if (CurrentCheckpoint == null) return;
             CurrentCheckpoint._player.Revive(CurrentCheckpoint._spawnPosition);
+            Debug.Log("Respawned player at " + CurrentCheckpoint._spawnPosition);
         }
+
+        public void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<Player>() == null) return;
+            OnCheckpointChange?.Invoke();
+            CurrentCheckpoint = this;
+            _spawnPosition = this.transform.position;
+            _player = other.GetComponent<Player>();
+            
+            Debug.Log("New checkpoint at " + _spawnPosition);
     
+        }
     }
 }
+
