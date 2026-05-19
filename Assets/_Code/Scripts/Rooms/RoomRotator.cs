3208a839cdd3c3337589a95995ef57b6331174e7
ShadowLoker <lluckoomecebria@gmail.com>
2026-05-19 17:21:26 +0200

    Room Hologram

--- Assets/_Code/Scripts/Rooms/RoomRotator.cs
+++ Assets/_Code/Scripts/Rooms/RoomRotator.cs
@@ -2,19 +2,75 @@
 using UnityEngine;
 using Interactions;
 using System.Collections;
+using UnityEditor;
 
 namespace Rooms
 {
     public class RoomRotator : MonoBehaviour, IHoldInteractable, ILockable
     {
-        [SerializeField] private Room targetRoom;
-        [SerializeField] private float rotationCooldown = 1.5f;
-
+        [SerializeField] public Room targetRoom;
+        [SerializeField] public float rotationCooldown = 1.5f;
+        [SerializeField] public MeshFilter hologramTarget;
+        
         private IInteractor _currentInteractor;
 
         public void Awake()
         {
             if (targetRoom == null) Debug.LogWarning("RoomRotator does not have a targetRoom", this);
+            if (hologramTarget == null) Debug.LogWarning("RoomRotator does not have a hologramPivot", this);
+            if (hologramTarget.sharedMesh == null) CreateHologramMesh();
+        }
+
+        public void CreateHologramMesh(bool saveAsset = true, bool makeNewInstance = true, bool optimize = true)
+        {
+            if (targetRoom == null) return;
+            if (hologramTarget == null) return;
+            
+            var meshFilters = targetRoom.gameObject.GetComponentsInChildren<MeshFilter>();
+            if (meshFilters.Length == 0) return;
+            var combine = new CombineInstance[meshFilters.Length];
+            for (int i = 0; i < meshFilters.Length; i++)
+            {
+                if (meshFilters[i].sharedMesh == null) continue;
+                combine[i].mesh = meshFilters[i].sharedMesh;
+                combine[i].transform =targetRoom.transform.worldToLocalMatrix *
+                                       meshFilters[i].transform.localToWorldMatrix;
+            }
+
+            var mesh = new Mesh();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            mesh.CombineMeshes(combine, true, true);
+
+            var center = mesh.bounds.center;
+            var verts = mesh.vertices;
+            for (int i = 0; i < verts.Length; i++)
+                verts[i] -= center;
+            mesh.vertices = verts;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            
+            hologramTarget.mesh = mesh;
+            hologramTarget.transform.rotation = targetRoom.transform.rotation;
+            
+            if (saveAsset)
+                SaveMesh(hologramTarget.sharedMesh, targetRoom.name + "_Hologram", makeNewInstance, optimize);
+            
+            Debug.Log("Hologram mesh created");
+        }
+
+        public void SaveMesh(Mesh mesh, string meshName, bool makeNewInstance, bool optimize)
+        {
+            string path = EditorUtility.SaveFilePanel("Save Hologram Mesh Asset", "Assets/", meshName, "asset");
+            if (string.IsNullOrEmpty(path)) return;
+            
+            path = FileUtil.GetProjectRelativePath(path);
+            
+            Mesh meshToSave = (makeNewInstance) ? Instantiate(mesh) : mesh;
+            
+            if (optimize) meshToSave.Optimize();
+            
+            AssetDatabase.CreateAsset(meshToSave, path);
+            AssetDatabase.SaveAssets();
         }
 
         public void RotateRoom()
