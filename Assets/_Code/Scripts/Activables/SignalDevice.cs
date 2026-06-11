3eb515a4d77e99d1c691e7e233e14b27e92f7925
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-06-12 00:35:46 +0200

    Signal device animation

---
+++ Assets/_Code/Scripts/Activables/SignalDevice.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SignalDevice : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Activate()
+    {
+        _animator.Play("send_signal", 0, 0f);
+    }
+}
