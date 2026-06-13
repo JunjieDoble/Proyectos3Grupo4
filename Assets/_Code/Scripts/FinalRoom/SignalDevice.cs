fff2fbfcad88dbff91cdac2983c7e46183308965
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-06-13 22:27:31 +0200

    finalroom script animation start with light path

--- Assets/_Code/Scripts/FinalRoom/SignalDevice.cs
+++ Assets/_Code/Scripts/FinalRoom/SignalDevice.cs
@@ -1,57 +1,30 @@
 ﻿using _Code.Scripts.Bases;
-using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class SignalDevice : Activable
 {
-    private Animator _animator;
-    private Animator _roofAnimator;
-    private Animator _hologramAnimator;
     private SignalVFX _signalVFX;
     private SignalParticleSystem _signalParticleSystem;
 
     private void Start()
     {
-        _animator = GetComponent<Animator>();
-        _roofAnimator = GameObject.Find("final_techo").GetComponent<Animator>();
-        _hologramAnimator = GameObject.Find("final_hologram").GetComponent<Animator>();
         _signalVFX = GetComponentInChildren<SignalVFX>();
         _signalParticleSystem = GetComponentInChildren<SignalParticleSystem>();
 
-        if (_signalVFX == null || _animator == null || _roofAnimator == null || _hologramAnimator == null || _signalParticleSystem == null )
+        if (_signalVFX == null || _signalParticleSystem == null )
         {
             Debug.LogError("SignalDevice is missing required components!");
         }
     }
 
-    public override void ActivatorUpdate()
-    {
-        if (IsActive())
-        {
-            Activate();
-        }
-    }
-
-    public void Activate()
-    {
-        _animator.Play("send_signal", 0, 0f);
-        _roofAnimator.Play("open_roof", 0, 0f);
-        _hologramAnimator.Play("hologram_on", 0, 0f);
-    }
-
-    public void OnSignalAnimationFinished()
+    public void OnSignalParticleStart()
     {
         _signalParticleSystem.Activate();
-        
-        StartCoroutine(StartSignalVFX());
     }
 
-    private IEnumerator StartSignalVFX()
+    public void OnSignalVFXStart()
     {
-        yield return new WaitForSeconds(4f);
-
-        //_signalParticleSystem.Deactivate();
         _signalVFX.Activate();
     }
 }
