507c2cd3a943ba06a41675c937e7f7b2a3138743
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-06-13 18:41:46 +0200

    Final room first test

---
+++ Assets/_Code/Scripts/FinalRoom/SignalDevice.cs
@@ -0,0 +1,62 @@
+﻿using _Code.Scripts.Bases;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SignalDevice : Activable
+{
+    private Animator _animator;
+    private Animator _roofAnimator;
+    private Animator _hologramAnimator;
+    private SignalVFX _signalVFX;
+    private SignalParticleSystem _signalParticleSystem;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _roofAnimator = GameObject.Find("final_techo").GetComponent<Animator>();
+        _hologramAnimator = GameObject.Find("final_hologram").GetComponent<Animator>();
+        _signalVFX = GetComponentInChildren<SignalVFX>();
+        _signalParticleSystem = GetComponentInChildren<SignalParticleSystem>();
+
+        if (_signalVFX == null || _animator == null || _roofAnimator == null || _hologramAnimator == null || _signalParticleSystem == null )
+        {
+            Debug.LogError("SignalDevice is missing required components!");
+        }
+
+        // Temporal para testing, remove this later
+        //_animator.Play("send_signal", 0, 0f);
+        //_roofAnimator.Play("open_roof", 0, 0f);
+        //_hologramAnimator.Play("hologram_on", 0, 0f);
+    }
+
+    public override void ActivatorUpdate()
+    {
+        if (IsActive())
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        _animator.Play("send_signal", 0, 0f);
+        _roofAnimator.Play("open_roof", 0, 0f);
+        _hologramAnimator.Play("hologram_on", 0, 0f);
+    }
+
+    public void OnSignalAnimationFinished()
+    {
+        _signalParticleSystem.Activate();
+        
+        StartCoroutine(StartSignalVFX());
+    }
+
+    private IEnumerator StartSignalVFX()
+    {
+        yield return new WaitForSeconds(4f);
+
+        //_signalParticleSystem.Deactivate();
+        _signalVFX.Activate();
+    }
+}
---
+++ Assets/_Code/Scripts/FinalRoom/SignalVFX.cs
@@ -0,0 +1,20 @@
+﻿using _Code.Scripts.Bases;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class SignalVFX : MonoBehaviour
+{
+    private VisualEffect _vfx;
+
+    private void Start()
+    {
+        _vfx = GetComponent<VisualEffect>();
+    }
+
+    public void Activate()
+    {
+        _vfx.SendEvent("OnStartBeam");    
+    }
+}
