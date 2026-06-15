2d19688a109a8372ef2122134b568ca91a6c5797
ShadowLoker <lluckoomecebria@gmail.com>
2026-06-15 06:00:01 +0200

    More changes

--- Assets/_Code/Scripts/Gameplay/MinimapCamera.cs
+++ Assets/_Code/Scripts/Gameplay/MinimapCamera.cs
@@ -6,6 +6,7 @@ public class MinimapCamera : MonoBehaviour
     [Header("References")]
     [SerializeField] private Camera minimapCamera;
     [SerializeField] private Transform player;
+    [SerializeField] private LayerMask checkMask;
 
     private float nearPlaneTarget;
 
@@ -24,7 +25,7 @@ public class MinimapCamera : MonoBehaviour
     private float CalculateNearClipPlane()
     {
         Ray ray = new Ray(player.position + Vector3.up, player.up);
-        if (Physics.Raycast(ray, out RaycastHit hit, (player.position - transform.position).magnitude, LayerMask.GetMask("Default")))
+        if (Physics.Raycast(ray, out RaycastHit hit, (player.position - transform.position).magnitude, checkMask))
         {
             return transform.position.y - hit.point.y;
         }
